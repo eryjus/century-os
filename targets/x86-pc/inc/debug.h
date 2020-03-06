8760d4bf87239fd4fe9d3d3b32663504d816c2a3
Adam Clark <hobbyos@eryjus.com>
2020-03-05 19:56:29 -0800

    Clean up some unneeded files

--- targets/x86-pc/inc/debug.h
+++
@@ -1,9 +0,0 @@
-#define DEBUG_TOKEN_PASTE(x) DEBUG_##x
-#define DEBUG_ENABLED(func) DEBUG_TOKEN_PASTE(func)>0
-#define DEBUG_MmuMapToFrame 1
-#define DEBUG_LApicBroadcastIpi 1
-#define DEBUG_IpiHandleTlbFlush 1
-#define DEBUG_PicBroadcastIpi 1
-#define DEBUG_PicMaskIrq 1
-#define DEBUG_PicUnmaskIrq 1
-#define DEBUG_IsrHandler 1
